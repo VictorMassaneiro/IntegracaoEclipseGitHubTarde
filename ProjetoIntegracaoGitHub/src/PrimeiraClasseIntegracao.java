56993c1cca98d5ba4c88c5bcbd6938d3308cc913
VictorMassaneiro <massagol08@gmail.com>
2026-06-17 15:19:33 -0300

    Primeiro commit

---
+++ ProjetoIntegracaoGitHub/src/PrimeiraClasseIntegracao.java
@@ -0,0 +1,10 @@
+
+public class PrimeiraClasseIntegracao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Primeiro commit no github com eclipse");
+	}
+
+}
